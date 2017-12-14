40628d0cabf7da6ab4bd8d462c76eae670e68da7
pmg <pmmtmmg@gmail.com>
2017-12-14 02:36:58 -0600

    more stuff

--- app.py
+++ app.py
@@ -9,15 +9,15 @@ import os
 import urlparse
 import psycopg2
 
-urlparse.uses_netloc.append("postgres")
-url = urlparse.urlparse(os.environ["DATABASE_URL"])
-conn = psycopg2.connect(
- database=url.path[1:],
- user=url.username,
- password=url.password,
- host=url.hostname,
- port=url.port
-)
+# urlparse.uses_netloc.append("postgres")
+# url = urlparse.urlparse(os.environ["DATABASE_URL"])
+# conn = psycopg2.connect(
+#  database=url.path[1:],
+#  user=url.username,
+#  password=url.password,
+#  host=url.hostname,
+#  port=url.port
+# )
 
 app = Flask(__name__)
 
