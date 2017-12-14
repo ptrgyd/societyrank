d99ea2c9a17fe48055709393768af8122cb938f9
pmg <pmmtmmg@gmail.com>
2017-12-14 02:50:11 -0600

    more stuff

--- app.py
+++ app.py
@@ -9,15 +9,15 @@ import os
 import urlparse
 import psycopg2
 
-# urlparse.uses_netloc.append("postgres")
-# url = urlparse.urlparse(os.environ["DATABASE_URL"])
-# conn = psycopg2.connect(
-#  database=url.path[1:],
-#  user=url.username,
-#  password=url.password,
-#  host=url.hostname,
-#  port=url.port
-# )
+urlparse.uses_netloc.append("postgres")
+url = urlparse.urlparse(os.environ["DATABASE_URL"])
+conn = psycopg2.connect(
+ database=url.path[1:],
+ user=url.username,
+ password=url.password,
+ host=url.hostname,
+ port=url.port
+)
 
 app = Flask(__name__)
 
@@ -27,6 +27,7 @@ app.config['SECRET_KEY'] = 'secret'
 
 
 db = SQLAlchemy(app)
+# db = SQLAlchemy(os.environ["DATABASE_URL"])
 login_manager = LoginManager()
 login_manager.init_app(app)
 # login_manager.login_message = 'Didnt work'
